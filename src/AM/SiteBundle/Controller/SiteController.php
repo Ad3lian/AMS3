7708075f05d1d39e34ef3e8ae0d6c93aee00fe52
adrien martin <adrien-martin@hotmail.fr>
2018-06-03 22:13:29 +0200

    création du formulaire d'inscription et enregistrement des données

--- src/AM/SiteBundle/Controller/SiteController.php
+++ src/AM/SiteBundle/Controller/SiteController.php
@@ -4,9 +4,16 @@ namespace AM\SiteBundle\Controller;
 
 use AM\SiteBundle\Entity\AboutMe;
 use AM\SiteBundle\Entity\Posts;
+use AM\SiteBundle\Entity\Register;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class SiteController extends Controller
 {
@@ -95,8 +102,48 @@ class SiteController extends Controller
     {
 
     }
-    public function inscriptionAction()
+    public function inscriptionAction(Request $request)
     {
-        return $this->render('AMSiteBundle:Site:inscription.html.twig');
+        //Création de l'objet
+        $inscription = new Register();
+
+        //On crée le FormBuilder
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $inscription);
+
+        //On ajoute les champs
+        $formBuilder
+            ->Add('name',      TextType::class)
+            ->Add('firstname', TextType::class)
+            ->Add('email',     TextType::class)
+            ->Add('password',  TextType::class)
+            ->Add('save',      SubmitType::class)
+        ;
+
+        //génération du formulaire
+        $form = $formBuilder->getForm();
+
+        //si la requête est en POST
+        if($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+            if($form->isValid()) {
+                //On récupère l'EntityManager
+                $em = $this->getDoctrine()->getManager();
+
+                //On présiste l'entité
+                $em->persist($inscription);
+
+                //On flush tout ce qui est persisté
+                $em->flush();
+
+                $request->getSession()->getFlashBag()->add('notice', 'Inscription bien enregistrée.');
+
+                return $this->render('AMSiteBundle:Site:attenteValidation.html.twig');
+            }
+        }
+
+        return $this->render('AMSiteBundle:Site:inscription.html.twig', array(
+            'form' => $form->createView(),
+        ));
     }
 }
---
+++ src/AM/SiteBundle/Entity/Register.php
@@ -0,0 +1,228 @@
+<?php
+
+namespace AM\SiteBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Register
+ *
+ * @ORM\Table(name="users_temp")
+ * @ORM\Entity(repositoryClass="AM\SiteBundle\Repository\RegisterRepository")
+ */
+class Register
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=35)
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="firstname", type="string", length=35)
+     */
+    private $firstname;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="register_date", type="datetime")
+     */
+    private $registerDate;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="grade", type="integer")
+     */
+    private $grade;
+
+    /**
+     * Register constructor.
+     */
+    public function __construct()
+    {
+        $this->registerDate = new \DateTime();
+        $this->grade = 0;
+    }
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Register
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set firstname
+     *
+     * @param string $firstname
+     *
+     * @return Register
+     */
+    public function setFirstname($firstname)
+    {
+        $this->firstname = $firstname;
+
+        return $this;
+    }
+
+    /**
+     * Get firstname
+     *
+     * @return string
+     */
+    public function getFirstname()
+    {
+        return $this->firstname;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     *
+     * @return Register
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return Register
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set registerDate
+     *
+     * @param \DateTime $registerDate
+     *
+     * @return Register
+     */
+    public function setRegisterDate($registerDate)
+    {
+        $this->registerDate = $registerDate;
+
+        return $this;
+    }
+
+    /**
+     * Get registerDate
+     *
+     * @return \DateTime
+     */
+    public function getRegisterDate()
+    {
+        return $this->registerDate;
+    }
+
+    /**
+     * Set grade
+     *
+     * @param integer $grade
+     *
+     * @return Register
+     */
+    public function setGrade($grade)
+    {
+        $this->grade = $grade;
+
+        return $this;
+    }
+
+    /**
+     * Get grade
+     *
+     * @return int
+     */
+    public function getGrade()
+    {
+        return $this->grade;
+    }
+}
+
